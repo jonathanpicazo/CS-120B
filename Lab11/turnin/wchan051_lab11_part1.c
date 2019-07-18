f6c53c83b2d61762babec63dae5718f22cc84c74
Wayland Chang <wchan051@wch136-02.cs.ucr.edu>
2019-07-17 20:45:01 -0700

    shut up jp

---
+++ Lab11/turnin/wchan051_lab11_part1.c
@@ -0,0 +1,143 @@
+/*	Author: wchan051
+ *  Partner(s) Name: Jonathan Picazo and Wayland Chang
+ *	Lab Section:
+ *	Assignment: Lab 11  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+//#include "simAVRHeader.h"
+#include "Keypad.h"
+#include "scheduler.h"
+
+volatile unsigned char TimerFlag = 0;
+unsigned long _avr_timer_M = 1; 
+unsigned long _avr_timer_cntcurr = 0; 
+
+  void TimerOn() {
+	  TCCR1B = 0x0B;
+	  OCR1A = 125;
+	  TIMSK1 = 0x02; 
+	  TCNT1 = 0;
+	  _avr_timer_cntcurr = _avr_timer_M;
+	  SREG |= 0x80;
+  }
+
+  void TimerOff() {
+	  TCCR1B = 0x00;
+  }
+
+  void TimerISR() {
+	  TimerFlag = 1;
+  }
+
+  ISR(TIMER1_COMPA_vect) {
+	  _avr_timer_cntcurr--;
+	  if (_avr_timer_cntcurr == 0) {
+		  TimerISR();
+		  _avr_timer_cntcurr = _avr_timer_M;
+	  }
+  }
+
+  void TimerSet(unsigned long M) {
+	  _avr_timer_M = M;
+	  _avr_timer_cntcurr = _avr_timer_M;
+  }
+
+
+unsigned char tmpB = 0x00;
+
+enum SM1_States{SM1_output};
+
+int SMTick1(int state){
+	unsigned char x;
+	x = GetKeypadKey();
+	switch(state){
+		case SM1_output:
+			switch (x) {
+				case '\0': tmpB = 0x1F; break; // All 5 LEDs on
+				case '1': tmpB = 0x01; break; // hex equivalent
+				case '2': tmpB = 0x02; break;
+				case '3': tmpB = 0x03; break;
+				case '4': tmpB = 0x04; break;
+				case '5': tmpB = 0x05; break;
+				case '6': tmpB = 0x06; break;
+				case '7': tmpB = 0x07; break;
+				case '8': tmpB = 0x08; break;
+				case '9': tmpB = 0x09; break;
+				case 'A': tmpB = 0x0A; break;
+				case 'B': tmpB = 0x0B; break;
+				case 'C': tmpB = 0x0C; break;
+				case 'D': tmpB = 0x0D; break;
+				case '*': tmpB = 0x0E; break;
+				case '0': tmpB = 0x00; break;
+				case '#': tmpB = 0x0F; break;
+				default: tmpB = 0x1B; break; // Should never occur. Middle LED off.
+			}
+			state = SM1_output;
+			PORTB=tmpB;
+			break;
+		}
+		return state;
+}
+
+
+int main(void)
+{
+	// Set Data Direction Registers
+	// Buttons PORTA[0-7], set AVR PORTA to pull down logic
+	DDRC = 0xFF; PORTC = 0x00;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRA = 0xF0; PORTA = 0x0F; // PC7..4 outputs init 0s, PC3..0 inputs init 1s
+	DDRD = 0xFF; PORTD = 0x00;
+	// Period for the tasks
+	unsigned long int SMTick1_calc = 20;
+
+
+	//Calculating GCD
+	unsigned long int tmpGCD = 10;
+
+	//Greatest common divisor for all tasks or smallest time unit for tasks.
+	unsigned long int GCD = tmpGCD;
+
+	//Recalculate GCD periods for scheduler
+	unsigned long int SMTick1_period = SMTick1_calc/GCD;
+
+	//Declare an array of tasks
+	static task task1;
+	task *tasks[] = { &task1};
+	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+
+	// Task 1
+	task1.state = 0;//Task initial state.
+	task1.period = SMTick1_period;//Task Period.
+	task1.elapsedTime = SMTick1_period;//Task current elapsed time.
+	task1.TickFct = &SMTick1;//Function pointer for the tick.
+
+
+	// Set the timer and turn it on
+	TimerSet(GCD);
+	TimerOn();
+
+	unsigned short i; // Scheduler for-loop iterator
+	while(1) {
+		// Scheduler code
+		for ( i = 0; i < numTasks; i++ ) {
+			// Task is ready to tick
+			if ( tasks[i]->elapsedTime == tasks[i]->period ) {
+				// Setting next state for task
+				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+				// Reset the elapsed time for next tick.
+				tasks[i]->elapsedTime = 0;
+			}
+			tasks[i]->elapsedTime += 1;
+		}
+		while(!TimerFlag);
+		TimerFlag = 0;
+	}
+
+	// Error: Program should not exit!
+	return 0;
+}
